253bb4413cf1e03a8f627a35a713eef68c7aecdf
Andres Alba <bryanalba92@gmail.com>
2019-08-19 15:53:12 -0500

    Creación de directiva del DropZone

--- src/app/components/carga/carga.component.ts
+++ src/app/components/carga/carga.component.ts
@@ -10,6 +10,7 @@ import { CargaImagenesService } from '../../services/carga-imagenes.service';
 })
 export class CargaComponent implements OnInit {
 
+  estaSobreElemento = false;
   archivos: FileItem[] = [];
 
   constructor(public _cargaImagenes: CargaImagenesService) { }
@@ -18,6 +19,12 @@ export class CargaComponent implements OnInit {
   }
   cargarImagenes() {
     this._cargaImagenes.cargarImagenesFirebase(this.archivos);
+  }
+
+  pruebaSobreElemento(event) {
+
+    console.log(event);
+
   }
 
 }
